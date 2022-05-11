7eafc33f9ce30ae8963004bf7db7e20d014c07fb
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-05-11 12:32:04 +0100

    completed task

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Write a method that determines if a given data set represents
+a valid UTF-8 encoding 
+"""
+
+
+def validUTF8(data):
+    """
+    function to validate utf-8 encoding
+    Args:
+        data(list): data set containing multiple characters
+    Returns:
+        boolean: true if the data is valid utf-8 encoded, otherwise false
+    """
+    utf8valid = 0
+    for val in data:
+        byte = val & 255
+        if utf8valid:
+            if byte >> 6 != 2:
+                return False
+            utf8valid -= 1
+            continue
+        while (1 << abs(7 - utf8valid)) & byte:
+            utf8valid += 1
+        if utf8valid == 1 or utf8valid > 4:
+            return False
+        utf8valid = max(utf8valid - 1, 0)
+    return utf8valid == 0
